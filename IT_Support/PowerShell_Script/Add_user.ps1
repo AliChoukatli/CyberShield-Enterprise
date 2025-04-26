0849a345b73022711c4484365720865b863fb968
Ali Choukatli <alichoukatli@gmail.com>
2025-04-26 11:21:04 -0400

    Update Add_user.ps1

--- IT_Support/PowerShell_Script/Add_user.ps1
+++ IT_Support/PowerShell_Script/Add_user.ps1
@@ -8,3 +8,23 @@ New-ADUser `
  -AccountPassword (ConvertTo-SecureString "TemporaryP@ssw0rd!" -AsPlainText -Force) `
  -Enabled $true `
  -ChangePasswordAtLogon $true
+
+
+
+-Name : Full display name.
+
+-GivenName : First name.
+
+-Surname : Last name.
+
+-SamAccountName : Username for authentication (logon name).
+
+-UserPrincipalName : Email format username (useful for Azure and cloud compatibility).
+
+-Path : The Organizational Unit (OU) where you create the user.
+
+-AccountPassword : Initial password (provided in plain text for the script).
+
+-Enabled : Immediately enables the account.
+
+-ChangePasswordAtLogon : Forces the user to change their password at first logon.
