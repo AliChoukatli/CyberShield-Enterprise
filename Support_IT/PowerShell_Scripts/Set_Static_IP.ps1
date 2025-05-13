92b92ea4bd83ff676d38259a41d7427c2b4eab77
Ali Choukatli <alichoukatli@gmail.com>
2025-05-13 16:57:05 -0400

    Delete Support_IT directory

--- Support_IT/PowerShell_Scripts/Set_Static_IP.ps1
+++
@@ -1,2 +0,0 @@
-New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress 192.168.2.10 -PrefixLength 24 -DefaultGateway 192.168.2.1
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ("127.0.0.1","8.8.8.8")
